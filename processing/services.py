ae20e19bee673fdd7513172e7e7b927c7395d2c1
Raphael Memmi <raphmemmi@outlook.com>
2025-02-27 20:45:30 +0000

    all sentences appended to a defaultdict

--- processing/services.py
+++ processing/services.py
@@ -1,6 +1,6 @@
 import os
 import spacy
-from collections import Counter
+from collections import Counter, defaultdict
 
 # load the spacy model (pre-trained model that can do everything we want)
 nlp = spacy.load("en_core_web_sm")
@@ -14,6 +14,10 @@ def process_text_files(directory="data/"):
 
     #dict to count word occurences
     word_counter = Counter()
+    # dict to map sentences to words
+    # we can append sentences without checking if they are already in the list
+    #defaultdict means that if a key is not found in the dictionary, it will create a new one automatically
+    sentence_map = defaultdict(list)
 
 
     # loop through all files in the directory
@@ -32,8 +36,16 @@ def process_text_files(directory="data/"):
                     words = [token.text.lower() for token in sent if token.is_alpha and not token.is_stop]
                     # update the dictionary with the new words currently in words (broken apart sentence by sentence)
                     word_counter.update(words)
+                    # for each word in the sentence, add the sentence to the list of sentences
+                    for word in words:
+                        sentence_map[word].append(sent.text)
 
+    print("\n🔹 Word Counter:")
     print(word_counter)
+
+    print("\n🔹 Sentence Map:")
+    for word, sentences in sentence_map.items():
+        print(f"{word}: {sentences}")
 
 if __name__ == "__main__":
     process_text_files()
